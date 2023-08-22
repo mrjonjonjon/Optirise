420232e9ac2eec9e85f148c14b6c02f127831677
Jonathan Akaba <jonathanakaba@jka.local>
2023-08-21 20:25:52 -0700

    small refactor

--- read_json.py
+++ read_json.py
@@ -26,43 +26,31 @@ for weapon_type in ['great_sword']:
 
     weapon_type_cap=process.extractOne(weapon_type, all_weapons_cap)[0]
 
-    for i in range(294):
+    for i in range(294):    
+        weapon_info_dict = parsed_data[weapon_type]['base_data']['param'][i]['base']
         id = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['base']['id'][weapon_type_cap]
+        name_key = 'name'
         if id >= 300:
             id = id % 300
-            name = parsed_data[weapon_type]['name_mr']['entries'][id]
-            realname = parsed_data[weapon_type]['name_mr']['entries'][id]['content'][1]
-            raw =               parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['atk']
-            affinity =          parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['critical_rate']
-            sharpness =         parsed_data[weapon_type]['base_data']['param'][i]['base']['sharpness_val_list']
-            def_bonus =         parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['def_bonus']
+            name_key = 'name_mr'
 
-            normal_deco_slots = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['slot_num_list']
-            rampage_deco_slots =parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_slot_num_list']
-            if parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list'][0]=="None" or \
-                (parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list'])==0:
-                continue
-            rampage_skill_ids = [d['Skill'] for d in parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list']]
-            
-            rampage_skill_names = [parsed_data['hyakuryu_skill_name_msg']['entries'][rid + 1]['content'][1] for rid in rampage_skill_ids]
+        name = parsed_data[weapon_type][name_key]['entries'][id]
+        realname = parsed_data[weapon_type][name_key]['entries'][id]['content'][1]
+        raw =               weapon_info_dict['base']['base']['atk']
+        affinity =          weapon_info_dict['base']['base']['critical_rate']
+        sharpness =         weapon_info_dict['sharpness_val_list']
+        def_bonus =         weapon_info_dict['base']['base']['def_bonus']
 
-            print(i,':::::',realname, rampage_skill_names, raw)
-        else:
-            name = parsed_data[weapon_type]['name']['entries'][id]
-            realname = parsed_data[weapon_type]['name']['entries'][id]['content'][1]
-            raw = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['atk']
-            affinity = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['critical_rate']
-            sharpness = parsed_data[weapon_type]['base_data']['param'][i]['base']['sharpness_val_list']
-            def_bonus =         parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['def_bonus']
+        normal_deco_slots = weapon_info_dict['base']['base']['slot_num_list']
+        rampage_deco_slots =weapon_info_dict['base']['base']['hyakuryu_slot_num_list']
+        if len(weapon_info_dict['base']['base']['hyakuryu_skill_id_list'])==0 or\
+        weapon_info_dict['base']['base']['hyakuryu_skill_id_list'][0]=="None":
+            continue
+        rampage_skill_ids = [d['Skill'] for d in parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list']]
+        
+        rampage_skill_names = [parsed_data['hyakuryu_skill_name_msg']['entries'][rid + 1]['content'][1] for rid in rampage_skill_ids]
 
-            normal_deco_slots = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['slot_num_list']
-            rampage_deco_slots = parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_slot_num_list']
-            if parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list'][0]=="None" or \
-                (parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list'])==0:
-                continue
-            rampage_skill_ids = [d['Skill'] for d in parsed_data[weapon_type]['base_data']['param'][i]['base']['base']['base']['hyakuryu_skill_id_list'] if d['Skill']!='None']
-            rampage_skill_names = [parsed_data['hyakuryu_skill_name_msg']['entries'][rid + 1]['content'][1] for rid in rampage_skill_ids]
-            print(i,'::::::',realname, rampage_skill_names, raw)
+        print(i,':::::',realname, rampage_skill_names, raw)
 
 #weapons start at line 2394640
 #use the weeapontype to number as id in  w thingy.
